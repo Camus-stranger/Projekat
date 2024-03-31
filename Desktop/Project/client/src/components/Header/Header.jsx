c0ef241d5ad039511e811de152296796b5c2c6bb
d.obradovic <153769348+Camus-stranger@users.noreply.github.com>
2024-03-31 16:13:16 +0200

    Gotov Projekat 1

--- Desktop/Project/client/src/components/Header/Header.jsx
+++ Desktop/Project/client/src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
 import "./Header.scss";
-import Search from "./Search/Search";
 import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
-    const [searchModal, setSearchModal] = useState(false);
     const navigate = useNavigate();
     const handleScroll = () => {
         const offset = window.scrollY;
@@ -36,13 +33,13 @@ const Header = () => {
                     <ul className="left">
                         <li onClick={() => navigate("/")}>Home</li>
                         <li onClick={() => navigate("/about")}>About</li>
-                        <li onClick={() => navigate("/")}>Categories</li>
+                        <li>Categories</li>
                     </ul>
                     <div className="center" onClick={() => navigate("/")}>
-                        DARKWOOD FURTNITURE STORE
+                        DARKWOOD FURNITURE STORE
                     </div>
                     <div className="right">
-                        <TbSearch onClick={() => setSearchModal(true)} />
+
                         <AiOutlineHeart />
                         <span
                             className="cart-icon"
@@ -54,7 +51,6 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-            {searchModal && <Search setSearchModal={setSearchModal} />}
             {showCart && <Cart />}
         </>
     );
